d5111c88f161fc5498ce5fe010befcbefe97e12f
vinixiusBR silva <marcosilva.vinicius09@gmail.com>
2024-06-17 21:10:45 +0000

    adicionada a função de inserir o resultado na tela

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,11 +1,49 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+import { verificarTema, trocarTema } from "../../helpers/tema-helpers.js";
 
 const botaoTema = document.querySelector(".tema button")
-const body = document.querySelector("body")
+const  body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
+const botaoJogarNovamente = document.querySelector("main button")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
-
 })
 
-verificarTema(body,botaoTema)
+botaoJogarNovamente.addEventListener("click", jogarNovamente)
+
+verificarTema(body, botaoTema)
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase)
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.tolowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
+
+function inserirResultado() {
+    const sectionPontuacao = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
+
+    sectionPontuacao.innerHTML = `
+               ${divAssunto.outerHTML}
+
+                <strong>${pontos}</strong>
+
+                <p>de 10</p>
+    `
+}
+
+function jogarNovamente() {
+    localStorage.removeItem
+    
+    window.location.href = "../../index.html"
+}
+
+inserirResultado()
