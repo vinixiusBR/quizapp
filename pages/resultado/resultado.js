290dc36a145ef90673c0e4936ded789f202aaf31
vinixiusBR silva <marcosilva.vinicius09@gmail.com>
2024-06-17 21:29:50 +0000

    ajustes da função de selecionar assunto para funcionar em todos os dispositivos

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,30 +1,14 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helpers.js";
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
-const  body = document.querySelector("body")
-const assunto = localStorage.getItem("assunto")
-const botaoJogarNovamente = document.querySelector("main button")
+const body = document.querySelector("body")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
+
 })
 
-botaoJogarNovamente.addEventListener("click", jogarNovamente)
-
-verificarTema(body, botaoTema)
-
-function alterarAssunto() {
-    const divIcone = document.querySelector(".assunto_icone")
-    const iconeImg = document.querySelector(".assunto_icone img")
-    const assuntoTitulo = document.querySelector(".assunto h1")
-
-    divIcone.classList.add(assunto.toLowerCase)
-    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.tolowerCase()}.svg`)
-    iconeImg.setAttribute("alt", `icone de ${assunto}`)
-    assuntoTitulo.innerText = assunto
-}
-
-alterarAssunto()
+verificarTema(body,botaoTema)
 
 function inserirResultado() {
     const sectionPontuacao = document.querySelector(".pontuacao")
--- script.js
+++ script.js
@@ -23,3 +23,5 @@ function selecionarAssunto(evento) {
 
 }
 
+
+
