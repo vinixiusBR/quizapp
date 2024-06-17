cd916d4c8ab7644d680a6e8560094ce044c6fb37
vinixiusBR silva <marcosilva.vinicius09@gmail.com>
2024-06-17 13:42:48 +0000

    adicionado o java script para a pagina de resultado

---
+++ pages/resultado/resultado.js
@@ -0,0 +1,11 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+
+})
+
+verificarTema(body,botaoTema)
